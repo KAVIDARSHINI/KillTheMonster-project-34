0e382b2bc2163b5891560390ddbe3550e0871c60
KAVIDARSHINI <76558960+KAVIDARSHINI@users.noreply.github.com>
2021-05-02 22:01:01 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -11,7 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("white");
+      fill(0, 96, 182);
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
